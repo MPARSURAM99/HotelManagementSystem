52123992fa2b618954f96370716d2aaa189fb378
Parsuram Maharana <123405783+MPARSURAM99@users.noreply.github.com>
2024-11-08 20:28:49 +0530

    error fixed

--- src/main/java/in/cutm/model/Location.java
+++ src/main/java/in/cutm/model/Location.java
@@ -2,14 +2,16 @@ package in.cutm.model;
 
 public class Location {
 	String branchName;
+	String branchPassword;
 
 	public Location() {
 		super();
 	}
 
-	public Location(String branchName) {
+	public Location(String branchName, String branchPassword) {
 		super();
 		this.branchName = branchName;
+		this.branchPassword = branchPassword;
 	}
 
 	public String getBranchName() {
@@ -19,6 +21,13 @@ public class Location {
 	public void setBranchName(String branchName) {
 		this.branchName = branchName;
 	}
-	
+
+	public String getBranchPassword() {
+		return branchPassword;
+	}
+
+	public void setBranchPassword(String branchPassword) {
+		this.branchPassword = branchPassword;
+	}
 	
 }
